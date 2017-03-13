9505fb2851aa5d1e485f4c5926020e5bba32d2b0
wanyu0409 <jasperwendy0409@gmail.com>
2017-03-13 14:46:52 +0800

    Shoes

---
+++ src/oo/abstraction/Shoes.java
@@ -0,0 +1,14 @@
+package oo.abstraction;
+
+public class Shoes {
+	String brand;
+	String color;
+	String function;
+	float size;
+	public Shoes(String brand, String color, String function, float size){
+		this.brand = brand;
+		this.color = color;
+		this.function = function;
+		this.size = size;
+	}
+}
---
+++ src/oo/abstraction/ShoesTester.java
@@ -0,0 +1,9 @@
+package oo.abstraction;
+
+public class ShoesTester {
+
+	public static void main(String[] args) {
+		Shoes s1 = new Shoes("Vans","Black","休閒",7);
+	}
+
+}
