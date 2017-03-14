3f3e7a73665963d4541d39975fe7bd3b7e7b6237
wanyu0409 <jasperwendy0409@gmail.com>
2017-03-14 15:57:02 +0800

    新增建構子(Construction)

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -1,5 +1,15 @@
 package oo.abstraction;
 
 public class Car {
-
+	String brand;
+	int cc;
+	String name;
+	String type;
+	int status;
+	
+	public Car(String brand, String name, int cc){
+		this.brand = brand;
+		this.name = name;
+		this.cc = cc;
+	}
 }
--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,8 +3,9 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Car c1 = new Car("TOYOTA", "ALTIS", 1800);
+		Car c2 = new Car("HONDA", "ACCORD", 2000);
+		Car c3 = new Car("TOYOTA", "Wish", 2000);
 	}
 
 }
