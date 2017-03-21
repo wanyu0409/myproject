e37b112e5411fe32c70e75dd80834541c0a0b900
wanyu0409 <jasperwendy0409@gmail.com>
2017-03-21 16:23:59 +0800

    Fields, Car id

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -6,6 +6,21 @@ public class CarTester {
 		Car c1 = new Car("TOYOTA", "ALTIS", 1800);
 		Car c2 = new Car("HONDA", "ACCORD", 2000);
 		Car c3 = new Car("TOYOTA", "Wish", 2000);
+		
+		int[] n = new int[5];
+		System.out.println(n[2]);
+		
+		Car[] cars = new Car[3];
+		cars[0] = new Car("TOYOTA", "ALTIS", 1800);
+		cars[1] = new Car("HONDA", "ACCORD", 2000);
+		cars[2] = new Car("TOYOTA", "Wish", 2000);
+		System.out.println(cars[0].name);
+		
+		int i=0;
+		for(i=0 ; i<3; i++){
+			System.out.println(cars[i].name);
+		}
+		System.out.println(i);
 	}
 
 }
