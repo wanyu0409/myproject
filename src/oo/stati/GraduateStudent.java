8dbeed04ab7bd500d30143b484902b13c2fa216f
wanyu0409 <jasperwendy0409@gmail.com>
2017-05-02 15:09:49 +0800

    GraudateStudent add

--- src/oo/stati/GraduateStudent.java
+++ src/oo/stati/GraduateStudent.java
@@ -3,6 +3,8 @@ package oo.stati;
 public class GraduateStudent extends Student {
 	int thesis;
 	public GraduateStudent (int english, int math, int chinese){
-		
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
 	}
 }
--- src/oo/stati/Student.java
+++ src/oo/stati/Student.java
@@ -10,15 +10,15 @@ class Student {
 		//不寫public、private、protected只能在這個package裡使用
 	}
 	//static類別變數，應用上用得到才用
-	//static{
-		//System.out.println("STATIC!!");
-		//System.out.println(english);
+	/*static{
+		System.out.println("STATIC!!");
+		System.out.println(english);
 		//因為在static區塊是物件還沒生成就執行，所以english不能再static區塊執行		
-		//System.out.println(pass);
+		System.out.println(pass);
 		//一切都還沒發生就有資料了 所以在static區塊就能使用他
 		
 		//在static類別裡不能使用this	
-	//}
+	}*/
 	
 	public Student(int english, int math, int chinese){
 		super();
